0dd78a0199f2c41dae8cab3f7e112cb729c8baa4
Zhi Han <zhi.han@gmail.com>
2013-04-17 16:54:50 -0400

    Added affine

--- src/main/scala/polynomial.scala
+++ src/main/scala/polynomial.scala
@@ -2,6 +2,8 @@ package my.polynomial
 
 import scala.annotation._
 import scala.math._
+import scala.collection.immutable.Map
+
 case class Rational(n:Int, d:Int)
 {
   @tailrec private def gcd(x:Int, y:Int): Int = 
@@ -25,9 +27,50 @@ case class Rational(n:Int, d:Int)
   def -(that:Rational) = new Rational(num * that.den - den *that.num ,den*that.den)
   def *(that:Rational) = new Rational(num * that.num, den * that.den)
   def /(that:Rational) = new Rational(num * that.den, den * that.num)
+  def isZero: Boolean = (num == 0)
 }
 
 object RationalConversion {
   // Implicit conversion from integer
   implicit def fromInt(x:Int) = Rational(x, 1)
-}  
+}
+
+case class Var(val name:String) {
+}
+
+// Affine forms ax + b 
+case class Affine (vars:Map[Var,Rational], val b:Rational) {
+  private def normalize(s:Map[Var,Rational]):Map[Var,Rational] = {
+    s.filter {case (_,a) => !a.isZero }
+  }
+
+  val A = normalize(vars)
+
+  override def toString = A.toList.map{
+    case(v,e) => e.toString + v.name.toString}.mkString("+") + 
+      " + " + b.toString 
+
+  def +(that:Affine): Affine = {
+    val a = for ((v,e) <- that.A ) yield (
+      if (A.contains(v)) v-> (A(v) + that.A(v)) else v-> that.A(v))
+    Affine(A ++ a, b + that.b)
+  }
+
+  def +(that:Rational): Affine = Affine(A, b + that)
+  
+  def -(that:Rational): Affine = Affine(A, b - that)
+  def -(that:Affine): Affine = this + that* Rational(-1,1)
+
+  def *(that:Rational): Affine = Affine(A.mapValues(e => e * that), b * that)
+  def *(that:Affine): Affine = if (that.A.isEmpty) 
+      this*that.b 
+    else 
+      throw new RuntimeException("Nonlinear")
+  
+  def /(that:Rational):Affine = Affine(A.mapValues(e => e / that), b /that)
+  def /(that:Affine): Affine = if (that.A.isEmpty) 
+      this/that.b
+    else
+      throw new RuntimeException("Nonlinear") 
+  
+} 
