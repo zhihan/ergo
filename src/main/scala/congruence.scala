5b0bd384e9aad9ff4d78abbdb3f58c0b523124bb
Zhi Han <zhi.han@gmail.com>
2013-04-22 10:55:10 -0400

    Temp add some methods to cc

--- src/main/scala/congruence.scala
+++ src/main/scala/congruence.scala
@@ -10,22 +10,32 @@ import my.uf._
 import my.ergo._
 
 // Every step of the inference rule produces a context
-class Context[T<:Rep[T]] (
-  val use:Map[T,TreeSet[HashedTerm]], 
-  val uf:UF[T]) {
+class Context[T<:Rep[T]] (val use:Map[T, TreeSet[HashedTerm]], val uf: UF[T]) {
   
   private def matchArg(t1:HashedTerm, t2:HashedTerm)(implicit fac:Factory[T]) = 
-    uf.areEqual(t1,t2) || uf.areEqual(t2, t1)
+    uf.areEqual(t1, t2) 
   
-  
-  private def matchList(f:Symbol, 
-    l1:List[HashedTerm], l2:List[HashedTerm]) (implicit fac:Factory[T]) = {
-    (l1 zip l2).forall( (x) => matchArg(x._1,x._2) )
+  private def matchList(l1:List[HashedTerm], 
+    l2:List[HashedTerm]) (implicit fac:Factory[T]) : Boolean = {
+    (l1 zip l2).forall({ case(x,y) => matchArg(x,y) })
   }
   
-  def congruent(u1:Term, u2:Term):Boolean = {
-   false 
+  // Decide whether two terms are known to be congruent
+  def congruent(u1:HashedTerm, u2:HashedTerm) (implicit fac:Factory[T]):Boolean = {
+    // Compare the symbol of the two terms
+    if (u1.t.f == u2.t.f) 
+      matchList(u1.t.xs, u2.t.xs)
+    else false
   }
+
+  def addTerm(t:HashedTerm): Context[T] = {
+    if (uf.contains(t)) this 
+    else {
+      // Add the term to the context
+      this
+    } 
+  }
+
 }
 
 object Context {
@@ -34,6 +44,4 @@ object Context {
   def empty[T<:Rep[T]] (fac:Factory[T]) = 
     new Context( Map[T,TreeSet[HashedTerm]](),UF.empty[T](fac))
   
-
-
 }
